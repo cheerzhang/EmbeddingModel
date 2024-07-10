961509ecd8f0ac6934c5468131ee0119c28cd681
cheerzhang <zhanglenus@gmail.com>
2024-07-10 10:44:43 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='emb-model',
+    name='emb-models',
     version='0.0.1',
     author="ZhangLe",
     author_email="zhangle@gmail.com",
