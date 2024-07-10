3c150fdfb801d517dbcc53fbc0e58c8dfa1ddffb
cheerzhang <zhanglenus@gmail.com>
2024-07-10 10:26:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,16 +6,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='statistic-tool',
+    name='emb-model',
     version='0.0.1',
     author="ZhangLe",
     author_email="zhangle@gmail.com",
-    description="simple functions for statistic",
+    description="simple useing for embedding models",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/cheerzhang/statistic_tool",
+    url="https://github.com/cheerzhang/EmbeddingModel",
     project_urls={
-        "Bug Tracker": "https://github.com/cheerzhang/statistic_tool/issues",
+        "Bug Tracker": "https://github.com/cheerzhang/EmbeddingModel/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
