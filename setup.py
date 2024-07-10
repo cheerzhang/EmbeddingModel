6268767bd6940dae688a188641964cc98ec79198
cheerzhang <zhanglenus@gmail.com>
2024-07-10 11:04:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPLv3",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     packages=setuptools.find_packages("."),
