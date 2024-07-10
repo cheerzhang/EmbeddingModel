6df389da2aa5536bcf0ce1617bc07748bcaaad02
cheerzhang <zhanglenus@gmail.com>
2024-07-10 13:34:15 +0200

    add create_char_to_idx

--- emb_model/__init__.py
+++ emb_model/__init__.py
@@ -3,8 +3,9 @@ from .customized_dataset import CDataset
 from .customized_dataset import ProcessJson
 from .customized_dataset import ProcessFilter
 from .customized_dataset import PrcocessDate
+from .customized_dataset import create_char_to_idx
 
-__all__ = ['CDataset', 
+__all__ = ['CDataset', 'create_char_to_idx',
            'ProcessJson', 'ProcessFilter', 'PrcocessDate']
-__dataset__ = ['CDataset']
+__dataset__ = ['CDataset', 'create_char_to_idx']
 __fe__ = ['ProcessJson', 'ProcessFilter', 'PrcocessDate']
