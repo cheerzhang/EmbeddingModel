f3470d81405e52cf9dd8fb91ea297e34c21f45b1
cheerzhang <zhanglenus@gmail.com>
2024-07-16 14:31:22 +0200

    fix a import bug

--- emb_model/__init__.py
+++ emb_model/__init__.py
@@ -8,6 +8,7 @@ from .customized_dataset import ProcessAge
 from .customized_dataset import ProcessCombineFE
 from .customized_dataset import ProcessSplitFE
 from .customized_dataset import ProcessNorm
+from .customized_dataset import MergeDf
 from .customized_dataset import create_char_to_idx, max_len_report
 from .customized_dataset import CheckData
 from .customized_dataset import trainModel, CharTransformerModel
@@ -17,10 +18,10 @@ from .customized_dataset import get_version
 __all__ = ['CDataset', 'create_char_to_idx', 'max_len_report', 'get_version',
         'ProcessJson', 'ProcessFilter', 'ProcessStr', 'ProcessNumer',
         'ProcessAge', 'PrcocessDate', 'ProcessDInDate',
-        'ProcessCombineFE', 'ProcessSplitFE', 'ProcessNorm',
+        'ProcessCombineFE', 'ProcessSplitFE', 'ProcessNorm', 'MergeDf',
         'CheckData', 'trainModel', 'CharTransformerModel']
 __dataset__ = ['CDataset', 'create_char_to_idx', 'max_len_report', 'CheckData']
 __fe__ = ['ProcessJson', 'ProcessFilter', 'ProcessStr', 'ProcessCombineFE', 
-          'ProcessAge', 'PrcocessDate', 'ProcessNumer', 'ProcessSplitFE',
+          'ProcessAge', 'PrcocessDate', 'ProcessNumer', 'ProcessSplitFE', 'MergeDf',
           'ProcessDInDate', 'ProcessNorm']
 __models__ = ['trainModel', 'CharTransformerModel']
