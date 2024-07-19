c15da24cf4de878916e1dc96d0f6ba316103ef4e
cheerzhang <zhanglenus@gmail.com>
2024-07-19 14:30:21 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='emb-model',
-    version='0.1.11',
+    version='0.1.12',
     author="ZhangLe",
     author_email="zhangle@gmail.com",
     description="simple useing for embedding models",
@@ -34,5 +34,5 @@ setuptools.setup(
         'pandas==2.1.4',
         'mlflow==2.11.3'
     ],
-    python_requires=">=3.11.2",
+    python_requires=">=3.9.0",
 )
